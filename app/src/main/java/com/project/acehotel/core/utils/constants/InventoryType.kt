5ba218bcb481cd0c3c74c23d5a805beec935270e
Krisna <krisnaerlangga08@gmail.com>
2023-12-27 10:36:10 +0700

    add: custom icon room and inventory

---
+++ app/src/main/java/com/project/acehotel/core/utils/constants/InventoryType.kt
@@ -0,0 +1,7 @@
+package com.project.acehotel.core.utils.constants
+
+enum class InventoryType(val type: String) {
+    LINEN("Linen"),
+    BED("Kasur"),
+    UNDEFINED("type")
+}
---
+++ app/src/main/java/com/project/acehotel/core/utils/constants/RoomType.kt
@@ -0,0 +1,7 @@
+package com.project.acehotel.core.utils.constants
+
+enum class RoomType(val type: String) {
+    REGULAR("Reguler"),
+    EXCLUSIVE("Exclusive"),
+    UNDEFINED("type")
+}
