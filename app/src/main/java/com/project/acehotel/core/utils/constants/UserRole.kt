5531ce3773139e40abc1589e8fdb637da1d265d3
Krisna <krisnaerlangga08@gmail.com>
2024-02-03 12:18:49 +0800

    add: update user role

--- app/src/main/java/com/project/acehotel/core/utils/constants/UserRole.kt
+++ app/src/main/java/com/project/acehotel/core/utils/constants/UserRole.kt
@@ -1,10 +1,11 @@
 package com.project.acehotel.core.utils.constants
 
-enum class UserRole(val role: String) {
-    MASTER("master"),
-    FRANCHISE("franchise"),
-    EMPLOYEE("employee"),
-    RECEPTIONIST("receptionist"),
-    ADMIN("admin"),
-    UNDEFINED("role")
+enum class UserRole(val role: String, val display: String) {
+    MASTER("owner", "Master"),
+    FRANCHISE("branch_manager", "Franchise"),
+    CLEANING_STAFF("cleaning_staff", "Cleaning"),
+    INVENTORY_STAFF("inventory_staff", "Inventory"),
+    RECEPTIONIST("receptionist", "Receptionist"),
+    ADMIN("admin", "Admin"),
+    UNDEFINED("role", "Role")
 }
