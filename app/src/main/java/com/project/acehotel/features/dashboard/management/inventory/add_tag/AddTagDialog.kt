26cf70637280f319811d9f208724bdf10dc8512d
Krisna <krisnaerlangga08@gmail.com>
2024-04-19 13:55:13 +0700

    add: check tag availability

--- app/src/main/java/com/project/acehotel/features/dashboard/management/inventory/add_tag/AddTagDialog.kt
+++ app/src/main/java/com/project/acehotel/features/dashboard/management/inventory/add_tag/AddTagDialog.kt
@@ -4,21 +4,21 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import com.project.acehotel.R
 import com.project.acehotel.core.data.source.Resource
+import com.project.acehotel.core.utils.showLongToast
 import com.project.acehotel.features.dashboard.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class AddTagDialog(private val id: String, private val inventorName: String) : DialogFragment() {
     private val addTagViewModel: AddTagViewModel by activityViewModels()
+
+    private lateinit var tagId: String
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity.let {
@@ -35,46 +35,68 @@ class AddTagDialog(private val id: String, private val inventorName: String) : D
             val addTagText = view?.findViewById<TextView>(R.id.tv_add_tag)
             addTagText?.text = "Apakah anda yakin ingin menyimpan data tag pada $inventorName"
 
+            setupTag()
+
             btnLogoutNo?.setOnClickListener {
                 dismiss()
             }
 
             btnLogoutYes?.setOnClickListener {
-                showLoading(true)
-
-                addTagViewModel.executeAddTag(READER_NAME, id).observe(requireActivity()) { tag ->
-                    when (tag) {
-                        is Resource.Error -> {
-                            showLoading(false)
-                            Timber.tag("AddTagDialog").e(tag.message)
-                        }
-                        is Resource.Loading -> {
-                            showLoading(true)
-                        }
-                        is Resource.Message -> {
-                            showLoading(false)
-                            Timber.tag("AddTagDialog").d(tag.message)
-
-                        }
-                        is Resource.Success -> {
-                            showLoading(false)
-
-                            val intentToMain = Intent(requireContext(), MainActivity::class.java)
-                            activity?.startActivity(intentToMain)
-                            dismiss()
-                        }
-                    }
-                }
+                handleAddTagButton()
             }
 
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
 
-    private fun showLoading(isLoading: Boolean) {
-        val loading = view?.findViewById<ProgressBar>(R.id.pb_add_tag)
+    private fun handleAddTagButton() {
+        addTagViewModel.addTag(tagId, id).observe(this@AddTagDialog) { tag ->
+            when (tag) {
+                is Resource.Error -> {
+                    dismiss()
+                    activity?.showLongToast("Gagal menambahkan tag")
+                }
+                is Resource.Loading -> {
 
-        loading?.visibility = if (isLoading) View.VISIBLE else View.GONE
+                }
+                is Resource.Message -> {
+                    dismiss()
+                    activity?.showLongToast("Gagal menambahkan tag")
+                }
+                is Resource.Success -> {
+
+                    activity?.showLongToast("Berhasil menambahkan tag")
+                    val intentToMain = Intent(requireContext(), MainActivity::class.java)
+                    activity?.startActivity(intentToMain)
+                    dismiss()
+                }
+            }
+        }
+    }
+
+    private fun setupTag() {
+        addTagViewModel.getTagById(READER_NAME).observe(this@AddTagDialog) { tag ->
+            when (tag) {
+                is Resource.Error -> {
+                    dismiss()
+                    activity?.showLongToast("Tag tidak terdeteksi")
+                }
+                is Resource.Loading -> {
+                    dismiss()
+                    activity?.showLongToast("Reader sedang mendeteksi tag")
+                }
+                is Resource.Message -> {
+                    dismiss()
+                    activity?.showLongToast("Tag tidak terdeteksi")
+                }
+                is Resource.Success -> {
+                    if (tag.data != null) {
+                        activity?.showLongToast("Tag berhasil terdeteksi")
+                        tagId = tag.data
+                    }
+                }
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/com/project/acehotel/features/dashboard/management/inventory/add_tag/AddTagViewModel.kt
+++ app/src/main/java/com/project/acehotel/features/dashboard/management/inventory/add_tag/AddTagViewModel.kt
@@ -1,10 +1,7 @@
 package com.project.acehotel.features.dashboard.management.inventory.add_tag
 
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.project.acehotel.core.data.source.Resource
-import com.project.acehotel.core.domain.inventory.model.Tag
 import com.project.acehotel.core.domain.inventory.usecase.InventoryUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -14,34 +11,34 @@ class AddTagViewModel @Inject constructor(
     private val inventoryUseCase: InventoryUseCase,
 ) : ViewModel() {
 
-    private fun getTagById(readerId: String) = inventoryUseCase.getTagById(readerId).asLiveData()
+    fun getTagById(readerId: String) = inventoryUseCase.getTagById(readerId).asLiveData()
 
-    private fun addTag(
+    fun addTag(
         tid: String,
         inventoryId: String
     ) = inventoryUseCase.addTag(tid, inventoryId).asLiveData()
 
-    fun executeAddTag(
-        readerId: String,
-        inventoryId: String
-    ): MediatorLiveData<Resource<Tag>> = MediatorLiveData<Resource<Tag>>().apply {
-        addSource(getTagById(readerId)) { tagId ->
-            when (tagId) {
-                is Resource.Error -> {
-
-                }
-                is Resource.Loading -> {
-
-                }
-                is Resource.Message -> {
-
-                }
-                is Resource.Success -> {
-                    addSource(addTag(tagId.data ?: "", inventoryId)) { tag ->
-                        value = tag
-                    }
-                }
-            }
-        }
-    }
+//    fun executeAddTag(
+//        readerId: String,
+//        inventoryId: String
+//    ): MediatorLiveData<Resource<Tag>> = MediatorLiveData<Resource<Tag>>().apply {
+//        addSource(getTagById(readerId)) { tagId ->
+//            when (tagId) {
+//                is Resource.Error -> {
+//
+//                }
+//                is Resource.Loading -> {
+//
+//                }
+//                is Resource.Message -> {
+//
+//                }
+//                is Resource.Success -> {
+//                    addSource(addTag(tagId.data ?: "", inventoryId)) { tag ->
+//                        value = tag
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
